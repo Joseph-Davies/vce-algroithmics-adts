6ac42e6f04de57164c615239197a6bcb5758bb2a
Joseph-Davies <daviesjd1@gmail.com>
2020-04-30 12:50:01 +1000

    added priority queue

--- ATDs.py
+++ ATDs.py
@@ -42,3 +42,49 @@ class queue:
 		output += "}"
 		return output
 
+class priority_queue:
+	def __init__(self):
+		self._container = list()
+	
+	def enqueue(self, element, priority):
+		packet = (element, priority)
+		
+		if len(self._container) == 0:
+			self._container.append(packet)
+			return
+		
+		i = 0
+		while i < len(self._container):
+			if self._container[i][1] > priority:
+				self._container.insert(i, packet)
+				break
+			i += 1
+	
+	def front(self):
+		return self._container[0]
+	
+	def back(self):
+		return self._container[-1]
+	
+	def length(self):
+		return len(self._container)
+	
+	def __len__(self):
+		return len(self._container)
+	
+	def __str__(self):
+		output = "priotiry queue {"
+		output += str(self._container[0])
+		for element in self._container[1:]:
+			output += ", " + str(element)
+		output += "}"
+		return output
+		
+	def __repr__(self):
+		output = "priotiry queue {"
+		output += str(self._container[0])
+		for element in self._container[1:]:
+			output += ", " + str(element)
+		output += "}"
+		return output
+
